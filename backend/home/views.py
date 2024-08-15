4a8eab6ed05ea6fe477c4dedd6bbed37adcc7cb9
drbea <beavoguimaximeakoi@gmail.com>
2024-08-15 08:18:21 -0400

    initialiation des premiers templates de base pour l'acceuil, insertion des lien static css, bootstrap ajout de l'application dans les settings du projet 'config'

---
+++ backend/home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home_page(request):
+
+    context = {
+        
+    }
+    return render(request, "home/index.html", context)
